282260d155c83e0141f70e5d630344bbd5abe301
koojunho <koojh@9cells.com>
2016-03-16 12:01:58 +0900

    대규모 수정 / 위험

--- SimpleBoardServiceProvider.php
+++ SimpleBoardServiceProvider.php
@@ -5,7 +5,7 @@ namespace NineCells\SimpleBoard;
 use App;
 use Illuminate\Contracts\Auth\Access\Gate as GateContract;
 use Illuminate\Support\ServiceProvider;
-use NineCells\Auth\AuthServiceProvider;
+use NineCells\Auth\MemberServiceProvider;
 use Mews\Purifier\PurifierServiceProvider;
 
 use NineCells\SimpleBoard\Console\CreateBoardCommand;
@@ -51,7 +51,7 @@ class SimpleBoardServiceProvider extends ServiceProvider
 
     public function register()
     {
-        App::register(AuthServiceProvider::class);
+        App::register(MemberServiceProvider::class);
         App::register(PurifierServiceProvider::class);
 
         $this->registerCreateBoardCommand();
